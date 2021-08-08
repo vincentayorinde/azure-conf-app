b5cc155da2a0e9c4b6ec806a1a7976d60cb8074f
Vincent Ayorinde <vincentayorinde@yahoo.com>
2021-08-08 10:32:57 +0100

    complete task

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from azure.servicebus import QueueClient
 
 
--- web/config.py
+++ web/config.py
@@ -4,19 +4,19 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="techconfdbserver3.postgres.database.azure.com"
+    POSTGRES_USER="azureuser@techconfdbserver3"
+    POSTGRES_PW="pa$$word1"
+    POSTGRES_DB="techconfdb3"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://notificationqueue1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+nKgWtJrfixHSJPSCw5QS+QOYBwZf5MbPTECBXbYxko=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
-
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
+    print(DB_URL);
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
